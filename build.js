82163421770b791416b1ba087c59312da788c190
Makis Tracend <makis.tracend@gmail.com>
2015-05-01 20:08:20 -0700

    renaming main

--- build.js
+++ build.js
@@ -53,7 +53,7 @@ function concat(opts) {
 			return _fs.readFileSync(filePath, FILE_ENCODING);
 		});
 
-	var wrapper = _fs.readFileSync('lib/scope.js', FILE_ENCODING);
+	var wrapper = _fs.readFileSync('lib/main.js', FILE_ENCODING);
 
 	var template = _handlebars.compile( wrapper );
 
